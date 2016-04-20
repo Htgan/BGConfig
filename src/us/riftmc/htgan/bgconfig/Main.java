528099192856e1ceb1caa07db534d1f92b2dc649
Htgan <andyhan12345@gmail.com>
2016-04-20 10:40:47 +1000

    Added default config

--- src/us/riftmc/htgan/bgconfig/Main.java
+++ src/us/riftmc/htgan/bgconfig/Main.java
@@ -1,5 +1,9 @@
 package us.riftmc.htgan.bgconfig;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -8,7 +12,12 @@ public class Main {
 //	private String emailAddress;
 	
 	public static void main(String[] args) {
-		System.out.println(getClock().toString());
+		try {
+			String fullPath = ExportResource("/MG983G-30M.TXT");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private static ArrayList<String> getClock() {
@@ -26,4 +35,31 @@ public class Main {
 		
 		return clock;
 	}
+	
+    static public String ExportResource(String resourceName) throws Exception {
+        InputStream stream = null;
+        OutputStream resStreamOut = null;
+        String jarFolder;
+        try {
+            stream = Main.class.getResourceAsStream(resourceName);//note that each / is a directory down in the "jar tree" been the jar the root of the tree
+            if(stream == null) {
+                throw new Exception("Cannot get resource \"" + resourceName + "\" from Jar file.");
+            }
+
+            int readBytes;
+            byte[] buffer = new byte[4096];
+            jarFolder = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath().replace('\\', '/');
+            resStreamOut = new FileOutputStream(jarFolder + resourceName);
+            while ((readBytes = stream.read(buffer)) > 0) {
+                resStreamOut.write(buffer, 0, readBytes);
+            }
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            stream.close();
+            resStreamOut.close();
+        }
+
+        return jarFolder + resourceName;
+    }
 }
